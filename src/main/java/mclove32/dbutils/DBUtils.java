dd7a7b607c97a4f011ae5dec7a8ae77dad2efa16
azuazu3939 <98195581+azuazu3939@users.noreply.github.com>
2024-02-26 18:19:14 +0900

    Add files via upload

---
+++ src/main/java/mclove32/dbutils/DBUtils.java
@@ -0,0 +1,17 @@
+package mclove32.dbutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DBUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
